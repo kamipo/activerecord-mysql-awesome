42cdeaf72539db2929c6211ac63ea71d5745c475
Ryuta Kamizono <kamipo@gmail.com>
2015-01-29 12:43:53 +0900

    Move the code in `Mysql::Awesome` namespace
    Because a class definition conflicts other gem.
    See https://github.com/waka/activerecord-mysql-unsigned/pull/23

--- lib/activerecord-mysql-awesome/active_record/connection_adapters/abstract_mysql_adapter.rb
+++ lib/activerecord-mysql-awesome/active_record/connection_adapters/abstract_mysql_adapter.rb
@@ -3,27 +3,94 @@ require 'active_record/connection_adapters/abstract_mysql_adapter'
 module ActiveRecord
   module Mysql
     module Awesome
-      def type_to_sql(type, limit = nil, precision = nil, scale = nil, unsigned = false)
-        return "#{type_to_sql(type, limit, precision, scale)} unsigned" if unsigned && type != :primary_key
-        case type.to_s
-        when 'integer'
-          case limit
-          when nil, 4, 11; 'int'  # compatibility with MySQL default
-          else
-            super(type, limit, precision, scale)
-          end
-        when 'primary_key'
-          "#{type_to_sql(:integer, limit, precision, scale, unsigned)} auto_increment PRIMARY KEY"
-        when 'datetime', 'time'
-          return super(type, limit, precision, scale) unless precision
+      class ChangeColumnDefinition < Struct.new(:column, :name) #:nodoc:
+      end
 
-          native_type = native_database_types[type.to_sym][:name]
-          case precision
-          when 0..6; "#{native_type}(#{precision})"
-          else raise(ActiveRecordError, "No #{native_type} type has precision of #{precision}. The allowed range of precision is from 0 to 6")
+      class ColumnDefinition < ActiveRecord::ConnectionAdapters::ColumnDefinition
+        attr_accessor :auto_increment, :unsigned, :charset, :collation
+      end
+
+      class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
+        def initialize(types, name, temporary, options, as = nil)
+          super(types, name, temporary, options)
+          @as = as
+        end
+
+        def primary_key(name, type = :primary_key, options = {})
+          options[:auto_increment] ||= type == :bigint
+          super
+        end
+
+        def new_column_definition(name, type, options) # :nodoc:
+          column = super
+          column.auto_increment = options[:auto_increment]
+          column.unsigned  = options[:unsigned]
+          column.charset   = options[:charset]
+          column.collation = options[:collation]
+          column
+        end
+
+        private
+
+        def create_column_definition(name, type)
+          ColumnDefinition.new(name, type)
+        end
+      end
+
+      module SchemaCreation
+        def visit_AddColumn(o)
+          add_column_position!("ADD #{accept(o)}", column_options(o))
+        end
+
+        private
+
+        def visit_ColumnDefinition(o)
+          o.sql_type = type_to_sql(o.type.to_sym, o.limit, o.precision, o.scale, o.unsigned)
+          column_sql = "#{quote_column_name(o.name)} #{o.sql_type}"
+          add_column_options!(column_sql, column_options(o)) unless o.type == :primary_key
+          column_sql
+        end
+
+        def visit_ChangeColumnDefinition(o)
+          change_column_sql = "CHANGE #{quote_column_name(o.name)} #{accept(o.column)}"
+          add_column_position!(change_column_sql, column_options(o.column))
+        end
+
+        def column_options(o)
+          column_options = super
+          column_options[:first] = o.first
+          column_options[:after] = o.after
+          column_options[:auto_increment] = o.auto_increment
+          column_options[:primary_key] = o.primary_key
+          column_options[:charset]   = o.charset
+          column_options[:collation] = o.collation
+          column_options
+        end
+
+        def add_column_options!(sql, options)
+          if options[:charset]
+            sql << " CHARACTER SET #{options[:charset]}"
           end
-        else
-          super(type, limit, precision, scale)
+          if options[:collation]
+            sql << " COLLATE #{options[:collation]}"
+          end
+          if options[:primary_key] == true
+            sql << " PRIMARY KEY"
+          end
+          super
+        end
+
+        def add_column_position!(sql, options)
+          if options[:first]
+            sql << " FIRST"
+          elsif options[:after]
+            sql << " AFTER #{quote_column_name(options[:after])}"
+          end
+          sql
+        end
+
+        def type_to_sql(type, limit, precision, scale, unsigned = false)
+          @conn.type_to_sql(type.to_sym, limit, precision, scale, unsigned)
         end
       end
 
@@ -128,110 +195,30 @@ module ActiveRecord
           include TimeValueWithPrecision
         end
       end
-    end
-  end
 
-  module ConnectionAdapters
-    class AbstractMysqlAdapter < AbstractAdapter
-      prepend Mysql::Awesome
+      public
 
-      class Column < ConnectionAdapters::Column # :nodoc:
-        prepend Mysql::Awesome::Column
-      end
-
-      class ChangeColumnDefinition < Struct.new(:column, :name) #:nodoc:
-      end
-
-      class ColumnDefinition < ActiveRecord::ConnectionAdapters::ColumnDefinition
-        attr_accessor :auto_increment, :unsigned, :charset, :collation
-      end
-
-      class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
-        def initialize(types, name, temporary, options, as = nil)
-          super(types, name, temporary, options)
-          @as = as
-        end
-
-        def primary_key(name, type = :primary_key, options = {})
-          options[:auto_increment] ||= type == :bigint
-          super
-        end
-
-        def new_column_definition(name, type, options) # :nodoc:
-          column = super
-          column.auto_increment = options[:auto_increment]
-          column.unsigned  = options[:unsigned]
-          column.charset   = options[:charset]
-          column.collation = options[:collation]
-          column
-        end
-
-        private
-
-        def create_column_definition(name, type)
-          ColumnDefinition.new(name, type)
-        end
-      end
-
-      class SchemaCreation < AbstractAdapter::SchemaCreation
-        def visit_AddColumn(o)
-          add_column_position!("ADD #{accept(o)}", column_options(o))
-        end
-
-        private
-
-        def visit_ColumnDefinition(o)
-          sql_type = type_to_sql(o.type.to_sym, o.limit, o.precision, o.scale, o.unsigned)
-          column_sql = "#{quote_column_name(o.name)} #{sql_type}"
-          add_column_options!(column_sql, column_options(o)) unless o.type == :primary_key
-          column_sql
-        end
-
-        def visit_ChangeColumnDefinition(o)
-          change_column_sql = "CHANGE #{quote_column_name(o.name)} #{accept(o.column)}"
-          add_column_position!(change_column_sql, column_options(o.column))
-        end
-
-        def column_options(o)
-          column_options = super
-          column_options[:first] = o.first
-          column_options[:after] = o.after
-          column_options[:auto_increment] = o.auto_increment
-          column_options[:primary_key] = o.primary_key
-          column_options[:charset]   = o.charset
-          column_options[:collation] = o.collation
-          column_options
-        end
-
-        def add_column_options!(sql, options)
-          if options[:charset]
-            sql << " CHARACTER SET #{options[:charset]}"
+      def type_to_sql(type, limit = nil, precision = nil, scale = nil, unsigned = false)
+        return "#{type_to_sql(type, limit, precision, scale)} unsigned" if unsigned && type != :primary_key
+        case type.to_s
+        when 'integer'
+          case limit
+          when nil, 4, 11; 'int'  # compatibility with MySQL default
+          else
+            super(type, limit, precision, scale)
           end
-          if options[:collation]
-            sql << " COLLATE #{options[:collation]}"
-          end
-          if options[:primary_key] == true
-            sql << " PRIMARY KEY"
-          end
-          super
-        end
+        when 'primary_key'
+          "#{type_to_sql(:integer, limit, precision, scale, unsigned)} auto_increment PRIMARY KEY"
+        when 'datetime', 'time'
+          return super(type, limit, precision, scale) unless precision
 
-        def add_column_position!(sql, options)
-          if options[:first]
-            sql << " FIRST"
-          elsif options[:after]
-            sql << " AFTER #{quote_column_name(options[:after])}"
+          native_type = native_database_types[type.to_sym][:name]
+          case precision
+          when 0..6; "#{native_type}(#{precision})"
+          else raise(ActiveRecordError, "No #{native_type} type has precision of #{precision}. The allowed range of precision is from 0 to 6")
           end
-          sql
-        end
-
-        def type_to_sql(type, limit, precision, scale, unsigned = false)
-          @conn.type_to_sql(type.to_sym, limit, precision, scale, unsigned)
-        end
-
-        def quote_value(value, column)
-          column.sql_type ||= type_to_sql(column.type, column.limit, column.precision, column.scale, column.unsigned)
-          super
+        else
+          super(type, limit, precision, scale)
         end
       end
 
@@ -265,7 +252,7 @@ module ActiveRecord
       end
 
       def migration_keys
-        super + [:unsigned, :collation]
+        super | [:unsigned, :collation]
       end
 
       def table_options(table_name)
@@ -314,19 +301,34 @@ module ActiveRecord
         schema_creation.accept(ChangeColumnDefinition.new(cd, column.name))
       end
 
-      alias configure_connection_without_awesome configure_connection
+      private
+
       def configure_connection
         _config = @config
         if [':default', :default].include?(@config[:strict])
           @config = @config.deep_merge(variables: { sql_mode: :default })
         end
-        configure_connection_without_awesome
+        super
       ensure
         @config = _config
       end
 
       def create_table_definition(name, temporary = false, options = nil, as = nil) # :nodoc:
         TableDefinition.new(native_database_types, name, temporary, options, as)
+      end
+    end
+  end
+
+  module ConnectionAdapters
+    class AbstractMysqlAdapter < AbstractAdapter
+      prepend Mysql::Awesome
+
+      class Column < ConnectionAdapters::Column # :nodoc:
+        prepend Mysql::Awesome::Column
+      end
+
+      class SchemaCreation < AbstractAdapter::SchemaCreation
+        prepend Mysql::Awesome::SchemaCreation
       end
     end
   end
